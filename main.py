95207672c9bbb938575700677c7edc5a27bfa4a0
Miguel Oppermann <miguel.o@gmx.net>
2024-03-30 00:40:10 +0100

    first release. older versions of sunshine with /pin on http GET dont work with latest moonlight clients so the only feature right now is toggling sunshine if installed

--- main.py
+++ main.py
@@ -1,41 +1,101 @@
-import os
-
-# The decky plugin module is located at decky-loader/plugin
-# For easy intellisense checkout the decky-loader code one directory up
-# or add the `decky-loader/plugin` path to `python.analysis.extraPaths` in `.vscode/settings.json`
 import decky_plugin
 
+import os
+import subprocess
+import signal
+
+from urllib.request import urlopen, Request
+
+def find_process_ids_by_name(name):
+    child = subprocess.Popen(['pgrep', '-f', name], stdout=subprocess.PIPE, shell=False)
+    response = child.communicate()[0]
+    return [int(pid) for pid in response.split()]
 
 class Plugin:
+    sunshine_pipe = None
+
     # A normal method. It can be called from JavaScript using call_plugin_function("method_1", argument1, argument2)
-    async def add(self, left, right):
-        return left + right
+    async def test(self):
+        return 3
+    
+    async def sunshineIsOnline(self):
+        decky_plugin.logger.info("Checking if Sunshine is running")
+        pid = find_process_ids_by_name("sunshine")
+        decky_plugin.logger.info("len(pid): " + str(len(pid)))
+        if (len(pid) > 0):
+            return True
+        return False
+    
+    async def sunshineStart(self):
+        decky_plugin.logger.info("Starting sunshine...")
+        if self.sunshine_pipe is not None:
+            self.sunshineStop()
+        try:
+            self.sunshine_pipe = subprocess.Popen("sh -c 'flatpak run dev.lizardbyte.app.Sunshine -0'", user=0, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, preexec_fn=os.setsid)
+        except:
+            decky_plugin.logger.info("exception thrown in subprocess.Popen... :c")
+            self.sunshine_pipe = None
+            return False
+        else:
+            decky_plugin.logger.info("started PID: " + str(self.sunshine_pipe.pid))
+            return True
+    
+    async def sunshineStop(self):
+        decky_plugin.logger.info("Stoping sunshine...")
+        try:
+            os.killpg(os.getpgid(self.sunshine_pipe.pid), signal.SIGTERM)
+            pids = find_process_ids_by_name("sunshine")
+            for pid in pids:
+                try:
+                    decky_plugin.logger.info("kill: " + str(pid))
+                    os.kill(pid, signal.SIGTERM)
+                except:
+                    decky_plugin.logger.info("error killing: " + str(pid))
+        except:
+            decky_plugin.logger.info("exception thrown in subprocess.terminate... :c")
+            return False
+        self.sunshine_pipe = None
+        return True
+    
+    async def sendPin(self, pin):
+        decky_plugin.logger.info("Sending PIN...")
+        try:
+            request = Request("http://localhost:47989/pin/" + pin)
+            with urlopen(request) as response:
+                json = response.read().decode()
+            decky_plugin.logger.info("PIN send")
+            return str(json)
+        except:
+            decky_plugin.logger.info("Error sending PIN")
+            return ""
 
     # Asyncio-compatible long-running code, executed in a task when the plugin is loaded
     async def _main(self):
-        decky_plugin.logger.info("Hello World!")
+        decky_plugin.logger.info("Decky Sunshine loaded")
 
     # Function called first during the unload process, utilize this to handle your plugin being removed
     async def _unload(self):
-        decky_plugin.logger.info("Goodbye World!")
+        decky_plugin.logger.info("Decky Sunshine unloaded")
         pass
 
     # Migrations that should be performed before entering `_main()`.
     async def _migration(self):
-        decky_plugin.logger.info("Migrating")
+        # check if installed or install sunshine
+
+        decky_plugin.logger.info("Migrating Decky Sunshine data")
         # Here's a migration example for logs:
-        # - `~/.config/decky-template/template.log` will be migrated to `decky_plugin.DECKY_PLUGIN_LOG_DIR/template.log`
+        # - `~/.config/decky-sunshine/sunshine.log` will be migrated to `decky_plugin.DECKY_PLUGIN_LOG_DIR/sunshine.log`
         decky_plugin.migrate_logs(os.path.join(decky_plugin.DECKY_USER_HOME,
-                                               ".config", "decky-template", "template.log"))
+                                               ".config", "decky-sunshine", "sunshine.log"))
         # Here's a migration example for settings:
-        # - `~/homebrew/settings/template.json` is migrated to `decky_plugin.DECKY_PLUGIN_SETTINGS_DIR/template.json`
-        # - `~/.config/decky-template/` all files and directories under this root are migrated to `decky_plugin.DECKY_PLUGIN_SETTINGS_DIR/`
+        # - `~/homebrew/settings/sunshine.json` is migrated to `decky_plugin.DECKY_PLUGIN_SETTINGS_DIR/sunshine.json`
+        # - `~/.config/decky-sunshine/` all files and directories under this root are migrated to `decky_plugin.DECKY_PLUGIN_SETTINGS_DIR/`
         decky_plugin.migrate_settings(
-            os.path.join(decky_plugin.DECKY_HOME, "settings", "template.json"),
-            os.path.join(decky_plugin.DECKY_USER_HOME, ".config", "decky-template"))
+            os.path.join(decky_plugin.DECKY_HOME, "settings", "sunshine.json"),
+            os.path.join(decky_plugin.DECKY_USER_HOME, ".config", "decky-sunshine"))
         # Here's a migration example for runtime data:
-        # - `~/homebrew/template/` all files and directories under this root are migrated to `decky_plugin.DECKY_PLUGIN_RUNTIME_DIR/`
-        # - `~/.local/share/decky-template/` all files and directories under this root are migrated to `decky_plugin.DECKY_PLUGIN_RUNTIME_DIR/`
+        # - `~/homebrew/sunshine/` all files and directories under this root are migrated to `decky_plugin.DECKY_PLUGIN_RUNTIME_DIR/`
+        # - `~/.local/share/decky-sunshine/` all files and directories under this root are migrated to `decky_plugin.DECKY_PLUGIN_RUNTIME_DIR/`
         decky_plugin.migrate_runtime(
-            os.path.join(decky_plugin.DECKY_HOME, "template"),
-            os.path.join(decky_plugin.DECKY_USER_HOME, ".local", "share", "decky-template"))
+            os.path.join(decky_plugin.DECKY_HOME, "sunshine"),
+            os.path.join(decky_plugin.DECKY_USER_HOME, ".local", "share", "decky-sunshine"))
--- src/index.tsx
+++ src/index.tsx
@@ -1,105 +1,114 @@
 import {
-  ButtonItem,
+  ToggleField,
   definePlugin,
-  DialogButton,
-  Menu,
-  MenuItem,
-  Navigation,
   PanelSection,
   PanelSectionRow,
   ServerAPI,
-  showContextMenu,
   staticClasses,
+  ButtonItem,
+  Spinner,
+  Navigation,
 } from "decky-frontend-lib";
-import { VFC } from "react";
-import { FaShip } from "react-icons/fa";
-
-import logo from "../assets/logo.png";
+import { VFC, useEffect, useState } from "react";
+import { FaSun } from "react-icons/fa";
+import { NumpadInput } from "./components/NumpadInput";
 
 // interface AddMethodArgs {
 //   left: number;
 //   right: number;
 // }
 
-const Content: VFC<{ serverAPI: ServerAPI }> = ({serverAPI}) => {
-  // const [result, setResult] = useState<number | undefined>();
+const __unused = (...argv: any[]) => { argv.toString() }
 
-  // const onClick = async () => {
-  //   const result = await serverAPI.callPluginMethod<AddMethodArgs, number>(
-  //     "add",
-  //     {
-  //       left: 2,
-  //       right: 2,
-  //     }
-  //   );
-  //   if (result.success) {
-  //     setResult(result.result);
-  //   }
-  // };
+const Content: VFC<{ serverAPI: ServerAPI }> = ({ serverAPI }) => {
+  const [sunshineIsEnabled, setSunshineIsEnabled] = useState<boolean>(false);
+  const [wantSetSunshineEnabled, setWantSetSunshineEnabled] = useState<boolean>(false);
+  const [pin, setPin] = useState("");
+  const [resPin, setResPin] = useState<any>();
 
-  return (
-    <PanelSection title="Panel Section">
-      <PanelSectionRow>
-        <ButtonItem
-          layout="below"
-          onClick={(e) =>
-            showContextMenu(
-              <Menu label="Menu" cancelText="CAAAANCEL" onCancel={() => {}}>
-                <MenuItem onSelected={() => {}}>Item #1</MenuItem>
-                <MenuItem onSelected={() => {}}>Item #2</MenuItem>
-                <MenuItem onSelected={() => {}}>Item #3</MenuItem>
-              </Menu>,
-              e.currentTarget ?? window
-            )
+  __unused(setSunshineIsEnabled)
+
+  const sendPin = async () => {
+    const result = await serverAPI.callPluginMethod<any, number>(
+      "sendPin",
+      {
+        pin
+      }
+    );
+    console.log("[SUN]", "sendPin result", result)
+    if (result.success) {
+      setResPin(result);
+    } else {
+      setResPin(undefined);
+    }
+  }
+
+  const sunshineCheckRunning = async () => {
+    const result = await serverAPI.callPluginMethod<any, number>(
+      "sunshineIsOnline",
+      {
+      }
+    );
+    console.log("[SUN]", "checkRunning result", result)
+    if (result.success) {
+      setSunshineIsEnabled(Boolean(result.result));
+    } else {
+      setSunshineIsEnabled(false);
+    }
+  };
+
+  useEffect(() => {
+    setWantSetSunshineEnabled(sunshineIsEnabled)
+  }, [sunshineIsEnabled])
+
+  useEffect(() => {
+    if (wantSetSunshineEnabled != sunshineIsEnabled) {
+      if (wantSetSunshineEnabled) {
+        console.log("[SUN]", "should enable")
+        serverAPI.callPluginMethod<any, number>(
+          "sunshineStart",
+          {
           }
-        >
-          Server says yolo
-        </ButtonItem>
-      </PanelSectionRow>
+        );
+      } else {
+        console.log("[SUN]", "should disable")
+        serverAPI.callPluginMethod<any, number>(
+          "sunshineStop",
+          {
+          }
+        );
+      }
+      window.setTimeout(() => sunshineCheckRunning(), 1000)
+    }
+  }, [wantSetSunshineEnabled])
 
-      <PanelSectionRow>
-        <div style={{ display: "flex", justifyContent: "center" }}>
-          <img src={logo} />
-        </div>
-      </PanelSectionRow>
+  sunshineCheckRunning()
 
+  return (wantSetSunshineEnabled != sunshineIsEnabled) ? <Spinner></Spinner> :
+    <PanelSection>
       <PanelSectionRow>
-        <ButtonItem
-          layout="below"
-          onClick={() => {
-            Navigation.CloseSideMenus();
-            Navigation.Navigate("/decky-plugin-test");
-          }}
-        >
-          Router
-        </ButtonItem>
+        <ToggleField
+          label="Enabled"
+          checked={wantSetSunshineEnabled}
+          onChange={setWantSetSunshineEnabled}
+        ></ToggleField>
       </PanelSectionRow>
+      {/* <NumpadInput value={pin} onChange={setPin} label="PIN"></NumpadInput>
+      <PanelSectionRow>
+        <ButtonItem onClick={() => sendPin()} disabled={pin.length < 4}>Send PIN</ButtonItem>
+      </PanelSectionRow>
+      <span>{JSON.stringify(resPin)}</span> */}
+      {/* {sunshineIsEnabled &&
+        <ButtonItem onClick={() => Navigation.NavigateToExternalWeb("http://127.0.0.1:7777")}>Web UI</ButtonItem>} */}
     </PanelSection>
-  );
-};
-
-const DeckyPluginRouterTest: VFC = () => {
-  return (
-    <div style={{ marginTop: "50px", color: "white" }}>
-      Hello World!
-      <DialogButton onClick={() => Navigation.NavigateToLibraryTab()}>
-        Go to Library
-      </DialogButton>
-    </div>
-  );
 };
 
 export default definePlugin((serverApi: ServerAPI) => {
-  serverApi.routerHook.addRoute("/decky-plugin-test", DeckyPluginRouterTest, {
-    exact: true,
-  });
-
   return {
-    title: <div className={staticClasses.Title}>Example Plugin</div>,
+    title: <div className={staticClasses.Title}>Decky Sunshine</div>,
     content: <Content serverAPI={serverApi} />,
-    icon: <FaShip />,
+    icon: <FaSun />,
     onDismount() {
-      serverApi.routerHook.removeRoute("/decky-plugin-test");
     },
   };
 });
