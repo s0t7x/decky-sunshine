02955dea83f5e58e6f60caf97f6e2b3401fbb011
Miguel Oppermann <miguel.o@gmx.net>
2024-04-07 00:04:08 +0200

    added working password input component

---
+++ src/components/PasswordInput.tsx
@@ -0,0 +1,28 @@
+// Basic React component that renders a numeric input field
+
+import React, { ChangeEvent, ChangeEventHandler, useEffect, useRef, useState } from "react";
+import { PanelSectionRow, gamepadDialogClasses, DialogButton, Focusable, TextField } from "decky-frontend-lib";
+
+// NumericInputProps interface with value and onChange properties
+interface PasswordInputProps {
+  label: string;
+  value: string;
+  onChange: (e: string) => void;
+}
+
+export const PasswordInput = (props: PasswordInputProps): JSX.Element => {
+  const { label, value, onChange } = props;
+
+  const tfId = Math.trunc(Math.random() * 999_999)
+
+  const doOnChange = (e: any) => {
+    e.target.type = "password"
+    onChange(e.target.value)
+  }
+
+  return (
+    <React.Fragment>
+      <TextField id={`passwordInput-${tfId}`} typeof="password" itemType="password" label={label} bIsPassword={true} bShowClearAction={value.length > 0} value={value} onChange={doOnChange}></TextField>
+    </React.Fragment>
+  );
+}
