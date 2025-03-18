86302bcd16a6b212e4106adf1f9df6b61ec291bd
Sebastian Kieritz <sebastian.kieritz@adesso.de>
2025-03-18 11:56:12 +0100

    Refactoring: Code cleanup

--- main.py
+++ main.py
@@ -10,24 +10,23 @@ from sunshine import SunshineController
 class Plugin:
     sunshineController = None
     settingManager = None
-    freshInstallation = False
-    
+
     async def set_setting(self, key, value):
         return self.settingManager.setSetting(key, value)
 
     async def get_setting(self, key, default):
         return self.settingManager.getSetting(key, default)
-        
+
     async def sunshineIsRunning(self):
         isRunning = self.sunshineController.isRunning()
         decky_plugin.logger.info("Is Sunshine running: " + str(isRunning))
         return isRunning
-    
+
     async def sunshineIsAuthorized(self):
         isAuthorized = self.sunshineController.isAuthorized()
         decky_plugin.logger.info("Is Decky Sunshine authorized: " + str(isAuthorized))
         return isAuthorized
-    
+
     async def sunshineStart(self):
         decky_plugin.logger.info("Starting sunshine...")
         res = self.sunshineController.start()
@@ -35,28 +34,30 @@ class Plugin:
             decky_plugin.logger.info("Sunshine started")
             self.settingManager.setSetting("lastRunState", "start")
         return res
-        
+
     async def sunshineStop(self):
         decky_plugin.logger.info("Stopping sunshine...")
         self.sunshineController.stop()
         self.settingManager.setSetting("lastRunState", "stop")
         return True
-    
-    async def sunshineIsFreshInstallation(self):
-        return self.freshInstallation
-    
+
     async def sunshineSetUser(self, newUsername, newPassword, confirmNewPassword, currentUsername = None, currentPassword = None):
+        if len(newUsername) + len(newPassword) < 1:
+            decky_plugin.logger.info("No User to set")
+            return
         decky_plugin.logger.info("Set Sunshine User...")
         result = self.sunshineController.setUser(newUsername, newPassword, confirmNewPassword, currentUsername, currentPassword)
-        decky_plugin.logger.info("User changed: " + str(result))
-        return result
-    
+        self.settingManager.setSetting("lastAuthHeader", str(result))
+        wasUserChanged = result is None
+        decky_plugin.logger.info("User changed: " + str(wasUserChanged))
+        return wasUserChanged
+
     async def sendPin(self, pin):
         decky_plugin.logger.info("Sending PIN..." + pin)
         send = self.sunshineController.sendPin(pin)
         decky_plugin.logger.info("PIN send " + str(send))
         return send
-    
+
     async def setAuthHeader(self, username, password):
         if len(username) + len(password) < 1:
             decky_plugin.logger.info("No AuthHeader to set")
@@ -69,39 +70,32 @@ class Plugin:
     async def _main(self):
         if self.sunshineController is None:
             self.sunshineController = SunshineController()
+
         if self.settingManager is None:
             decky_plugin.logger.info("Reading settings...")
             self.settingManager = SettingsManager(name = "decky-sunshine", settings_directory=os.environ["DECKY_PLUGIN_SETTINGS_DIR"])
             self.settingManager.read()
             decky_plugin.logger.info(str(self.settingManager))
-        if not self.sunshineController.isInstalled():
-            decky_plugin.logger.info("Sunshine is not installed. Installing...")
-            installed = self.sunshineController.install()
-            if installed:
-                self.freshInstallation = True
-                decky_plugin.logger.info("Sunshine installed")
-        if not self.sunshineController.isBwrapInstalled():
-            decky_plugin.logger.info("Decky Sunshine's copy of bwrap is missing. Obtaining now...'")
-            installed = self.sunshineController.installBwrap()
-            if installed:
-                self.freshInstallation = True
-                decky_plugin.logger.info("Decky Sunshine's copy of bwrap is obtained.")
-        if self.freshInstallation is True:
+
+        if not self.sunshineController.ensureDependencies():
+            return
+
+        if self.sunshineController.isFreshInstallation:
             self.sunshineController.start()
             triesLeft = 5
             time.sleep(2)
             while not self.sunshineController.isRunning() or triesLeft < 1:
                 triesLeft -= 1
                 time.sleep(1)
-            self.sunshineController.setUser("decky_sunshine", "decky_sunshine", "decky_sunshine")
-            self.sunshineController.setAuthHeader("decky_sunshine", "decky_sunshine")
+            res = self.sunshineController.setUser("decky_sunshine", "decky_sunshine", "decky_sunshine")
+            self.settingManager.setSetting("lastAuthHeader", str(res))
+            decky_plugin.logger.info("AuthHeader set " + res)
         else:
             lastAuthHeader = self.settingManager.getSetting("lastAuthHeader", "")
             decky_plugin.logger.info("lastAuthHeader: " + str(lastAuthHeader))
             if(len(lastAuthHeader) > 0):
                 self.sunshineController.setAuthHeaderRaw(lastAuthHeader)
-            decky_plugin.logger.info("Sunshine is installed")
-            decky_plugin.logger.info("Decky Sunshine's bwrap is installed")
+
         lastRunState = self.settingManager.getSetting("lastRunState", "")
         if(lastRunState == "start"):
             time.sleep(20)
